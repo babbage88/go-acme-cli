0001846e1c354b2bbc378b97352923604bbf7718
jtrahan <justin@trahan.dev>
2025-02-12 20:03:21 -0600

    reorganize sub-commands

--- commands/clodflare.go
+++ commands/clodflare.go
@@ -13,6 +13,8 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
+const versionNumber = "v1.0.0"
+
 var logger = pretty.NewCustomLogger(os.Stdout, "DEBUG", 1, "|", true)
 
 type GoInfraCli struct {
@@ -33,7 +35,7 @@ func (c *GoInfraCli) GetDnsSubCommands(sub *cli.Command) error {
 	start := len(c.BaseCommand.Commands)
 	c.BaseCommand.Commands = append(c.BaseCommand.Commands, sub)
 	if len(c.BaseCommand.Commands) == start {
-		err := fmt.Errorf("Error adding SubCommands, length %d did not change", start)
+		err := fmt.Errorf("error adding SubCommands, length %d did not change", start)
 		return err
 	}
 	return nil
@@ -91,12 +93,20 @@ func GetCloudFlareZoneIdForDomainName(envfile string, domainName string) (string
 }
 
 func printDnsRecordsTable(records []cloudflare.DNSRecord) {
-	tw := tabwriter.NewWriter(os.Stdout, 5, 1, 2, '\t', 0)
+	tw := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
 	fmt.Fprintf(tw, "%s\t%s\t%s\t%s\t%s\t%s\n", "ID", "Name", "Content", "Type", "CreatedOn", "ModifiedOn")
 	for _, v := range records {
 		pretty.Print(fmt.Sprintf("DNSRecord %s\t%s\t%s\t%s\t%s\t%s\n", v.ID, v.Name, v.Content, v.Type, pretty.DateTimeSting(v.CreatedOn), pretty.DateTimeSting(v.ModifiedOn)))
 		fmt.Fprintf(tw, "%s\t%s\t%s\t%s\t%s\t%s\n", v.ID, v.Name, v.Content, v.Type, pretty.DateTimeSting(v.CreatedOn), pretty.DateTimeSting(v.ModifiedOn))
 	}
+}
+
+func printDnsAndZoneIdTable(domain string, zoneId string) error {
+	tw := tabwriter.NewWriter(os.Stdout, 5, 0, 1, ' ', tabwriter.AlignRight)
+	fmt.Fprintln(tw, "DomainName\t\tZoneID")
+	fmt.Fprintf(tw, "%s\t\t%s\n", domain, zoneId)
+	err := tw.Flush()
+	return err
 }
 
 type UrFaveCliDocumentationSucks struct {
@@ -145,14 +155,16 @@ func getZoneIdCmd(envfile string, domain string) error {
 	}
 	msg := fmt.Sprintf("Domain: %s ZoneId: %s", domain, zoneId)
 	logger.Info(msg)
+	printDnsAndZoneIdTable(domain, zoneId)
+
 	return nil
 }
 
 func GetDnsSubCommands() []*cli.Command {
-	dnsCmds := []*cli.Command{
+	dnsSubCmds := []*cli.Command{
 		{
-			Name:    "dns",
-			Version: "1.0.0",
+			Name:    "zone",
+			Version: versionNumber,
 			Aliases: []string{"get-zoneid"},
 			Authors: cfDnsComandAuthors(),
 			Flags:   cfDnsCommandFlags(),
@@ -167,8 +179,9 @@ func GetDnsSubCommands() []*cli.Command {
 		},
 		{
 			Name:    "list",
-			Version: "1.0.0",
+			Version: versionNumber,
 			Authors: cfDnsComandAuthors(),
+			Aliases: []string{"list-records"},
 			Flags:   cfDnsCommandFlags(),
 			Action: func(ctx context.Context, cmd *cli.Command) (err error) {
 				if cmd.NArg() == 0 {
@@ -192,16 +205,30 @@ func GetDnsSubCommands() []*cli.Command {
 			},
 		},
 	}
-	return dnsCmds
+	return dnsSubCmds
+}
+
+func DnsBaseCommand() []*cli.Command {
+	cmd := []*cli.Command{
+		{
+			Name:                  "dns",
+			EnableShellCompletion: true,
+			Version:               versionNumber,
+			Authors:               cfDnsComandAuthors(),
+			Commands:              GetDnsSubCommands(),
+		},
+	}
+	return cmd
 }
 
 func CoreInfraCommand() *cli.Command {
 	cmd := &cli.Command{
-		Name:    "goinfra",
-		Version: "v1.0.0",
-		Authors: cfDnsComandAuthors(),
+		Name:                  "goinfra",
+		EnableShellCompletion: true,
+		Version:               "v1.0.0",
+		Authors:               cfDnsComandAuthors(),
 		// Flags:    cfDnsCommandFlags(),
-		Commands: GetDnsSubCommands(),
+		Commands: DnsBaseCommand(),
 	}
 	return cmd
 }
