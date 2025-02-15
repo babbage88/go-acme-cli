ef461a582a1c8fbbff0615f3c2bdbde3431bdc1b
jtrahan <justin@trahan.dev>
2025-02-14 21:09:47 -0600

    Added dns parent back

--- commands/commands.go
+++ commands/commands.go
@@ -4,59 +4,11 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
-	"os"
-	"text/tabwriter"
 
 	"github.com/babbage88/go-acme-cli/internal/pretty"
 	"github.com/cloudflare/cloudflare-go"
 	"github.com/urfave/cli/v3"
 )
-
-func printDnsRecordsTable(records []cloudflare.DNSRecord) {
-	var colorInt int32 = 97
-	tw := tabwriter.NewWriter(os.Stdout, 2, 0, 1, ' ', 0)
-	fmt.Fprintf(tw, "\x1b[1;%dm%s\t%s\t%s\t%s\t%s\t%s\t%s\x1b[0m\n", colorInt, "ID", "Name", "Content", "Type", "CreatedOn", "ModifiedOn", "Comment")
-	fmt.Fprintf(tw, "\x1b[1;%dm--\t----\t-------\t----\t---------\t----------\t-------\x1b[0m\n", colorInt)
-	for _, v := range records {
-		switch v.Type {
-		case "A":
-			colorInt = int32(96)
-		case "CNAME":
-			colorInt = int32(92)
-		default:
-			colorInt = int32(97)
-		}
-		fmt.Fprintf(tw, "\x1b[1;%dm%s\t%s\t%s\t%s\t%s\t%s\t%s\x1b[0m\n", colorInt, v.ID, v.Name, v.Content, v.Type, pretty.DateTimeSting(v.CreatedOn), pretty.DateTimeSting(v.ModifiedOn), v.Comment)
-	}
-	tw.Flush()
-}
-
-func printDnsRecord(record cloudflare.DNSRecord) {
-	var colorInt int32 = 97
-
-	switch record.Type {
-	case "A":
-		colorInt = int32(96)
-	case "CNAME":
-		colorInt = int32(92)
-	default:
-		colorInt = int32(97)
-	}
-	tw := tabwriter.NewWriter(os.Stdout, 2, 0, 1, ' ', 0)
-	fmt.Fprintf(tw, "\x1b[1;%dm%s\t%s\t%s\t%s\t%s\t%s\t%s\x1b[0m\n", colorInt, "ID", "Name", "Content", "Type", "CreatedOn", "ModifiedOn", "Comment")
-	fmt.Fprintf(tw, "\x1b[1;%dm--\t----\t-------\t----\t---------\t----------\t-------\x1b[0m\n", colorInt)
-	fmt.Fprintf(tw, "\x1b[1;%dm%s\t%s\t%s\t%s\t%s\t%s\t%s\x1b[0m\n", colorInt, record.ID, record.Name, record.Content, record.Type, pretty.DateTimeSting(record.CreatedOn), pretty.DateTimeSting(record.ModifiedOn), record.Comment)
-	tw.Flush()
-}
-
-func printDnsAndZoneIdTable(domain string, zoneId string) error {
-	tw := tabwriter.NewWriter(os.Stdout, 5, 0, 1, ' ', tabwriter.AlignRight)
-	fmt.Fprintln(tw, "DomainName\t\tZoneID")
-	fmt.Fprintln(tw, "----------\t\t------")
-	fmt.Fprintf(tw, "%s\t\t%s\n", domain, zoneId)
-	err := tw.Flush()
-	return err
-}
 
 func DnsBaseCommand() []*cli.Command {
 	cmd := []*cli.Command{
@@ -66,6 +18,15 @@ func DnsBaseCommand() []*cli.Command {
 			Version:               versionNumber,
 			Authors:               cfDnsComandAuthors(),
 			Commands:              GetDnsSubCommands(),
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:    "domain-name",
+					Aliases: []string{"n"},
+					Value:   "trahan.dev",
+					Sources: cli.EnvVars("CF_DOMAIN_NAME"),
+					Usage:   "Cloudflare Zone Id to retrieve records for.",
+				},
+			},
 		},
 	}
 	return cmd
@@ -77,29 +38,18 @@ func CoreInfraCommand() *cli.Command {
 		EnableShellCompletion: true,
 		Version:               "v1.0.0",
 		Authors:               cfDnsComandAuthors(),
-		Flags:                 GlobalCommandFlags(),
-		Commands:              GetDnsSubCommands(),
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "env-file",
+				Aliases: []string{"e"},
+				Value:   ".env",
+				Usage:   ".env file to use to load Cloudflare API keys and Zone ID",
+			},
+		},
+		//Commands:              GetDnsSubCommands(),
+		Commands: DnsBaseCommand(),
 	}
 	return cmd
-}
-
-func GlobalCommandFlags() []cli.Flag {
-	flags := []cli.Flag{
-		&cli.StringFlag{
-			Name:    "domain-name",
-			Aliases: []string{"n"},
-			Value:   "trahan.dev",
-			Sources: cli.EnvVars("CF_DOMAIN_NAME"),
-			Usage:   "Cloudflare Zone Id to retrieve records for.",
-		},
-		&cli.StringFlag{
-			Name:    "env-file",
-			Aliases: []string{"e"},
-			Value:   ".env",
-			Usage:   ".env file to use to load Cloudflare API keys and Zone ID",
-		},
-	}
-	return flags
 }
 
 func cfDnsUpdateFlags() []cli.Flag {
---
+++ commands/print_functions.go
@@ -0,0 +1,56 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/babbage88/go-acme-cli/internal/pretty"
+	"github.com/cloudflare/cloudflare-go"
+)
+
+func printDnsRecordsTable(records []cloudflare.DNSRecord) {
+	var colorInt int32 = 97
+	tw := tabwriter.NewWriter(os.Stdout, 2, 0, 1, ' ', 0)
+	fmt.Fprintf(tw, "\x1b[1;%dm%s\t%s\t%s\t%s\t%s\t%s\t%s\x1b[0m\n", colorInt, "ID", "Name", "Content", "Type", "CreatedOn", "ModifiedOn", "Comment")
+	fmt.Fprintf(tw, "\x1b[1;%dm--\t----\t-------\t----\t---------\t----------\t-------\x1b[0m\n", colorInt)
+	for _, v := range records {
+		switch v.Type {
+		case "A":
+			colorInt = int32(96)
+		case "CNAME":
+			colorInt = int32(92)
+		default:
+			colorInt = int32(97)
+		}
+		fmt.Fprintf(tw, "\x1b[1;%dm%s\t%s\t%s\t%s\t%s\t%s\t%s\x1b[0m\n", colorInt, v.ID, v.Name, v.Content, v.Type, pretty.DateTimeSting(v.CreatedOn), pretty.DateTimeSting(v.ModifiedOn), v.Comment)
+	}
+	tw.Flush()
+}
+
+func printDnsRecord(record cloudflare.DNSRecord) {
+	var colorInt int32 = 97
+
+	switch record.Type {
+	case "A":
+		colorInt = int32(96)
+	case "CNAME":
+		colorInt = int32(92)
+	default:
+		colorInt = int32(97)
+	}
+	tw := tabwriter.NewWriter(os.Stdout, 2, 0, 1, ' ', 0)
+	fmt.Fprintf(tw, "\x1b[1;%dm%s\t%s\t%s\t%s\t%s\t%s\t%s\x1b[0m\n", colorInt, "ID", "Name", "Content", "Type", "CreatedOn", "ModifiedOn", "Comment")
+	fmt.Fprintf(tw, "\x1b[1;%dm--\t----\t-------\t----\t---------\t----------\t-------\x1b[0m\n", colorInt)
+	fmt.Fprintf(tw, "\x1b[1;%dm%s\t%s\t%s\t%s\t%s\t%s\t%s\x1b[0m\n", colorInt, record.ID, record.Name, record.Content, record.Type, pretty.DateTimeSting(record.CreatedOn), pretty.DateTimeSting(record.ModifiedOn), record.Comment)
+	tw.Flush()
+}
+
+func printDnsAndZoneIdTable(domain string, zoneId string) error {
+	tw := tabwriter.NewWriter(os.Stdout, 5, 0, 1, ' ', tabwriter.AlignRight)
+	fmt.Fprintln(tw, "DomainName\t\tZoneID")
+	fmt.Fprintln(tw, "----------\t\t------")
+	fmt.Fprintf(tw, "%s\t\t%s\n", domain, zoneId)
+	err := tw.Flush()
+	return err
+}
