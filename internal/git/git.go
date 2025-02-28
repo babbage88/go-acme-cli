f16e69048efae400fa9be38e48fce7cb144da16b
jtrahan <justin@trahan.dev>
2025-02-27 19:02:52 -0600

    update .gitignore

--- internal/git/git.go
+++ internal/git/git.go
@@ -1,9 +1,12 @@
 package git
 
 import (
+	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 func Clone(url string, dst string) error {
@@ -12,4 +15,23 @@ func Clone(url string, dst string) error {
 		Progress: os.Stdout,
 	})
 	return err
+}
+
+func GetTags(path string) ([]string, error) {
+	tags := make([]string, 0)
+	r, err := git.PlainOpen(path)
+	if err != nil {
+		slog.Error("error opening git repo", slog.String("error", err.Error()))
+		return tags, err
+	}
+	gtags, err := r.Tags()
+	if err != nil {
+		return tags, err
+	}
+	err = gtags.ForEach(func(t *object.Tag) error {
+		tags = append(tags, t.Name)
+		fmt.Println(t.Name)
+		return nil
+	})
+	return tags, err
 }
